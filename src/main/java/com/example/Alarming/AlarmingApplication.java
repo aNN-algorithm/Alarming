ded7532bcdd73a9a47f6041787ef18ed40d21daf
aNN-algorithm <kyukyu0314@naver.com>
2024-10-12 20:15:07 +0900

    🎉 Created Application

---
+++ src/main/java/com/example/Alarming/AlarmingApplication.java
@@ -0,0 +1,13 @@
+package com.example.Alarming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlarmingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Alarming/AlarmingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Alarming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
